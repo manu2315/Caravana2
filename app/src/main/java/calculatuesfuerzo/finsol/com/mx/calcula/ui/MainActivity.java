1abf19bdeffe47e960b82c3e6bff3ed8873e4be5
manu2315 <manuairalert@gmail.com>
2018-08-14 20:48:10 -0500

    Ya estoy al mismo nivel de ayer
    Al mismo nivel de ayer.

--- app/src/main/java/calculatuesfuerzo/finsol/com/mx/calcula/ui/MainActivity.java
+++ app/src/main/java/calculatuesfuerzo/finsol/com/mx/calcula/ui/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import calculatuesfuerzo.finsol.com.mx.calcula.R;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
