6dd3372143ce9a1587636d9141f1ea4fa1162ad5
manu2315 <manuairalert@gmail.com>
2018-08-15 08:38:21 -0500

    Se termino de crear todos los componentes para el Stepper
    Se termino drawable+scrolling+ButtomTab.
    Se termino de crear todos los componentes para el Stepper
    *Falta definir invocacion de ActivityDrawable+Stepper
    *Falta definir el conteiner de los fragments.
    *Falta crear las funciones para obtener nombre e imagen en drawable.
    *Falta crear el databinding.

---
+++ app/src/main/java/calculatuesfuerzo/finsol/com/mx/calcula/adapters/MyStepperAdapter.java
@@ -0,0 +1,141 @@
+package calculatuesfuerzo.finsol.com.mx.calcula.adapters;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
+
+
+import com.stepstone.stepper.Step;
+import com.stepstone.stepper.adapter.AbstractFragmentStepAdapter;
+import com.stepstone.stepper.viewmodel.StepViewModel;
+
+import calculatuesfuerzo.finsol.com.mx.calcula.R;
+import calculatuesfuerzo.finsol.com.mx.calcula.ui.fragments.Test1Fragment;
+import calculatuesfuerzo.finsol.com.mx.calcula.ui.fragments.Test2Fragment;
+import calculatuesfuerzo.finsol.com.mx.calcula.ui.fragments.Test3Fragment;
+
+public  class MyStepperAdapter extends AbstractFragmentStepAdapter {
+
+    private static final String CURRENT_STEP_POSITION_KEY = "messageResourceId";
+    public MyStepperAdapter(FragmentManager fm, Context context) {
+        super(fm, context);
+    }
+
+    @Override
+    public Step createStep(int position) {
+        switch (position){
+            case 0:
+                final Test1Fragment step1 = new Test1Fragment();
+                Bundle b1 = new Bundle();
+                b1.putInt(CURRENT_STEP_POSITION_KEY, position);
+                step1.setArguments(b1);
+                return step1;
+            case 1:
+                final Test2Fragment step2 = new Test2Fragment();
+                Bundle b2 = new Bundle();
+                b2.putInt(CURRENT_STEP_POSITION_KEY, position);
+                step2.setArguments(b2);
+                return step2;
+            case 2:
+                final Test3Fragment step3 = new Test3Fragment();
+                Bundle b3 = new Bundle();
+                b3.putInt(CURRENT_STEP_POSITION_KEY, position);
+                step3.setArguments(b3);
+                return step3;
+
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @NonNull
+    @Override
+    public StepViewModel getViewModel(@IntRange(from = 0) int position) {
+        //Override this method to set Step title for the Tabs, not necessary for other stepper types
+        StepViewModel.Builder builder = new StepViewModel.Builder(context);
+                //.setTitle("Hola Mundo!!");
+        //INICIO
+        switch (position){
+            case 0:
+                builder.setTitle("Cliente")
+                        .setEndButtonLabel("Siguiente")
+                        .setBackButtonLabel("Cancelar")
+                        .setBackButtonVisible(true);
+                       /* .setNextButtonEndDrawableResId(R.drawable.ic_arrow_forward_24dp)
+                        .setBackButtonStartDrawableResId(StepViewModel.NULL_DRAWABLE);*/
+
+                break;
+            case 1:
+                builder.setTitle("Dirección")
+                        .setEndButtonLabel("Siguiente")
+                        .setBackButtonLabel("Volver");
+                       /* .setNextButtonEndDrawableResId(R.drawable.ic_chevron_right_24dp)
+                        .setBackButtonStartDrawableResId(R.drawable.ic_arrow_back_24dp);*/
+
+                break;
+            case 2:
+                builder.setTitle("Teléfono")
+                        .setEndButtonLabel("Completar")
+                        .setBackButtonLabel("Volver");
+                        /*.setNextButtonEndDrawableResId(StepViewModel.NULL_DRAWABLE)
+                        .setBackButtonStartDrawableResId(R.drawable.ic_arrow_back_24dp);*/
+
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported position: " + position);
+        }
+       //FIN
+        return builder.create();
+
+
+        /*
+         StepViewModel.Builder builder = new StepViewModel.Builder(context)
+                .setTitle(R.string.tab_title);
+        switch (position) {
+            case 0:
+                builder
+                    .setEndButtonLabel("This way")
+                    .setBackButtonLabel("Cancel")
+                    .setNextButtonEndDrawableResId(R.drawable.ms_forward_arrow)
+                    .setBackButtonStartDrawableResId(StepViewModel.NULL_DRAWABLE);
+                break;
+            case 1:
+                builder
+                    .setEndButtonLabel(R.string.go_to_summary)
+                    .setBackButtonLabel("Go to first")
+                    .setBackButtonStartDrawableResId(R.drawable.ms_back_arrow);
+                break;
+            case 2:
+                builder
+                    .setBackButtonLabel("Go back")
+                    .setEndButtonLabel("I'm done!");
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported position: " + position);
+        }
+        return builder.create();
+        */
+
+        /*switch (position){
+            case 0:
+                return new StepViewModel.Builder(context)
+                        .setTitle("Cliente") //can be a CharSequence instead
+                        .create();
+            case 1:
+                return new StepViewModel.Builder(context)
+                        .setTitle("Dirección") //can be a CharSequence instead
+                        .create();
+            case 2:
+                return new StepViewModel.Builder(context)
+                        .setTitle("Teléfono") //can be a CharSequence instead
+                        .create();
+        }
+        return null;*/
+    }
+}
--- app/src/main/java/calculatuesfuerzo/finsol/com/mx/calcula/ui/MainActivity.java
+++ app/src/main/java/calculatuesfuerzo/finsol/com/mx/calcula/ui/MainActivity.java
@@ -2,9 +2,12 @@ package calculatuesfuerzo.finsol.com.mx.calcula.ui;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.SubtitleCollapsingToolbarLayout;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -22,35 +25,42 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
 
-    //INICIO TAB ACTIVITY
+    //INICIO VARIABLES
+    //BottomTab
     private TextView mTextMessage;
+    private BottomNavigationView navigation;
+    //Drawable
+    DrawerLayout drawer;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+    //Collapse ActionBar with ImageView Parallax Slide Animation
+    Toolbar toolbar;
+    AppBarLayout appBarLayout;
+    //SubtitleCollapsingToolbar
+    SubtitleCollapsingToolbarLayout collapsingToolbarLayout;
+    boolean ExpandedActionBar = true;
+    //fragments
+    public android.app.FragmentManager fragmentManager;
+    int idFm;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-    //FIN TAB ACTIVITY
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        bindUI();
+
+        //Pruebas
+        /*
+        collapsingToolbarLayout.setTitle("Marco Antonio Lopez Perez");
+        collapsingToolbarLayout.setSubtitle("1234567890-Asesor de Credito");*/
+    }
+
+    //Metodos propios
+    private void bindUI(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        collapsingToolbarLayout=(SubtitleCollapsingToolbarLayout)findViewById(R.id.ctoolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -62,28 +72,85 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         //INICIO TAB ACTIVITY
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         //FIN TAB ACTIVITY
     }
 
+    private void expand(){
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if(Math.abs(verticalOffset)>200){
+                    ExpandedActionBar=false;
+                    collapsingToolbarLayout.setTitle("Marco Antonio Lopez Perez");
+                    collapsingToolbarLayout.setSubtitle("1234567890-Asesor de Credito");
+                    invalidateOptionsMenu();
+                }else{
+                    ExpandedActionBar = true;
+                    collapsingToolbarLayout.setTitle("");//Marco Antonio Lopez Perez\n"+"1234567890-Asesor de Credito
+                    collapsingToolbarLayout.setSubtitle("");
+                    invalidateOptionsMenu();
+                }
+            }
+        });
+    }
+
+    private void setFragmentByDefault()
+    {
+        //HomeFragment fragment = new HomeFragment();
+        //Metodo para utilzarse con el menu de sandwich MenuItem item
+        //changeFragment(fragment, navigationView.getMenu().getItem(0));
+    }
+    //Metodo para utilzarse con el menu de sandwich MenuItem item
+    private void changeFragment(Fragment fragment, MenuItem item) {
+
+
+        //Intentarlo con content_main en el layout principal
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
+        //idFm=getSupportFragmentManager().getBackStackEntryCount();
+        item.setCheckable(true);
+        //getSupportActionBar().setTitle(item.getTitle());
+        getSupportActionBar().setTitle("Datos del Cliente");
+        drawer.closeDrawers();
+    }
+    /*
+    public void fragmentClienttelephone(){
+
+        fragmentManager=getFragmentManager();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        ClientTelephoneFragment fragment = new ClientTelephoneFragment();
+        idFm++;
+        getSupportActionBar().setTitle("Teléfonos del Cliente");
+        transaction.replace(R.id.content_frame,fragment).addToBackStack(null).commit();
+    }
+     */
+    //Metodos genericos
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
+        }else if(getSupportFragmentManager().getBackStackEntryCount()>0){
+            if(idFm>0) {
+                getSupportFragmentManager().popBackStack();
+                idFm--;
+            }else
+                super.onBackPressed();
+        }
+        else {
             super.onBackPressed();
         }
     }
@@ -110,6 +177,28 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    //BottomTab
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.add_prospect:
+                    return true;
+                case R.id.reports:
+                    return true;
+                case R.id.others:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
+    //Drawable
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -129,6 +218,32 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }
+        //Ejemplo de fragments con drawable
+         /*
+        boolean fragmentTransaction = false;
+         fragment = null;
+
+        //mNuEventos.setEnabled(false);
+
+        switch (item.getItemId()) {
+
+            case R.id.mnCalendario:
+                fragment = new CalendarFragment();
+                fragmentTransaction = true;
+                //idFm=31;
+                //fragmentID.push(idFm);
+                break;
+
+            case R.id.mnueventos:
+                fragment = new EventoListFragment();
+                fragmentTransaction = true;
+
+                //idFm =32;
+                //GPSstar();
+                //fragmentID.push(idFm);
+                break;
+
+                */
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
