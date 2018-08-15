19ba2f7c21daae233d3563e651140f849fe2962e
manu2315 <manuairalert@gmail.com>
2018-08-15 06:39:53 -0500

    Creados Drawable+scrolling+ButtomTab
    Se termino drawable+scrolling+ButtomTab.
    *Falta crear ActivityDrawable+Stepper
    *Falta definir el conteiner de los fragments.
    *Falta crear las funciones para obtener nombre e imagen en drawable.
    *Falta crear el databinding.

--- app/src/main/java/calculatuesfuerzo/finsol/com/mx/calcula/ui/MainActivity.java
+++ app/src/main/java/calculatuesfuerzo/finsol/com/mx/calcula/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package calculatuesfuerzo.finsol.com.mx.calcula.ui;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -12,11 +14,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import calculatuesfuerzo.finsol.com.mx.calcula.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    //INICIO TAB ACTIVITY
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+    //FIN TAB ACTIVITY
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +70,12 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //INICIO TAB ACTIVITY
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //FIN TAB ACTIVITY
     }
 
     @Override
